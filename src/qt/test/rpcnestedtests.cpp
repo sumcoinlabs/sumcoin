604aea18d5da4209ecf929b90956d2194a5752a0
sumcoinlabs <sumcoinlabs@gmail.com>
2019-12-12 01:26:47 -0700

    rpcnestedtests.cpp update

--- src/qt/test/rpcnestedtests.cpp
+++ src/qt/test/rpcnestedtests.cpp
@@ -75,7 +75,7 @@ void RPCNestedTests::rpcNestedTests()
     QVERIFY(result == result2);
 
     RPCConsole::RPCExecuteCommandLine(*node, result, "getblock(getbestblockhash())[tx][0]", &filtered);
-    QVERIFY(result == "97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9");
+    QVERIFY(result == "b82fb0f59328af96928f3a7648461f3db41fbfc2fef4e5ec6f7cf78ca067eacc");
     QVERIFY(filtered == "getblock(getbestblockhash())[tx][0]");
 
     RPCConsole::RPCParseCommandLine(nullptr, result, "importprivkey", false, &filtered);
