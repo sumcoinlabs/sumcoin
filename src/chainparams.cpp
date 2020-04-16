e38a6503b9eb6d92f133b35e57d59c9bb20944fc
Sumcoin <37975862+sumcoinlabs@users.noreply.github.com>
2020-04-16 01:21:02 -0600

    cp's to 1077120

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -114,10 +114,10 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1555788600; // September 5, 2018
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0x000000000000000000000000000000000000000000000000158472d379749c45"); // to block 1048320
+        consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000001698ceb7e42462e3"); // to block 1077120
 
         // By default assume that the signatures in ancestors of this block are valid.
-        consensus.defaultAssumeValid = uint256S("0xc12e7cae889b0dd49f6d3d39fc40ff7ec5aadbfffb20d6ac4cb461c05f88e638"); //1048320
+        consensus.defaultAssumeValid = uint256S("0x259ec9e319169f587e3f9f8e38a0bfd5ca0cbc33a1e7c05c51b9b93fd1d17458"); //1077120
 
         /**
          * The message start string is designed to be unlikely to occur in normal data.
@@ -222,10 +222,11 @@ public:
 		{990720, uint256S("0x7a14741c8b5c10eab3464cd23b65554f1bc5c940e2ccb69461802e61e4f73b96")},
 	       {1019520, uint256S("0xab7a59f310e2e86d81af0ca1f608d24127d8af934825c34ba3897eab1ab398cf")},
 	       {1048320, uint256S("0xc12e7cae889b0dd49f6d3d39fc40ff7ec5aadbfffb20d6ac4cb461c05f88e638")},
+	       {1077120, uint256S("0x259ec9e319169f587e3f9f8e38a0bfd5ca0cbc33a1e7c05c51b9b93fd1d17458")},
 
 		    
-		    //{1077120, uint256S("0x")},
-		    //{1105920,
+		    //{1134720, uint256S("0x")},
+		    //{1163520,
 		    
 
             }
