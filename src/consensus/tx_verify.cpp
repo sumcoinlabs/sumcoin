4b6c518bf14280b24a7fea4fac81e2a8d16b0d96
jakesum <57389504+jakesum@users.noreply.github.com>
2020-10-20 19:36:32 -0600

    maturity, subsidy, difficulty tx_verify
    to match mebagger 0.17 to 0.15

--- src/consensus/tx_verify.cpp
+++ src/consensus/tx_verify.cpp
@@ -222,7 +222,7 @@ bool Consensus::CheckTxInputs(const CTransaction& tx, CValidationState& state, c
 
             // If prev is coinbase, check that it's matured
             if (coin.IsCoinBase()) {
-                if (nSpendHeight - coin.nHeight < COINBASE_MATURITY)
+                if (coin.IsCoinBase() && nSpendHeight - coin.nHeight < Params().GetMaturity(coin.nHeight)->nCoinbaseMaturity) {
                     return state.Invalid(false,
                         REJECT_INVALID, "bad-txns-premature-spend-of-coinbase",
                         strprintf("tried to spend coinbase at depth %d", nSpendHeight - coin.nHeight));
