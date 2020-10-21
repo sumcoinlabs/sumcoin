bd2c92410dde6ddeed8fb0e64498059e1cc214ea
jakesum <57389504+jakesum@users.noreply.github.com>
2020-10-20 19:53:18 -0600

    update test_bitcoin.cpp
    match mebagger 0.17 to 0.15

--- src/test/test_bitcoin.cpp
+++ src/test/test_bitcoin.cpp
@@ -120,7 +120,7 @@ TestChain100Setup::TestChain100Setup() : TestingSetup(CBaseChainParams::REGTEST)
     // Generate a 100-block chain:
     coinbaseKey.MakeNewKey(true);
     CScript scriptPubKey = CScript() <<  ToByteVector(coinbaseKey.GetPubKey()) << OP_CHECKSIG;
-    for (int i = 0; i < COINBASE_MATURITY; i++)
+    for (int i = 0; i < Params().GetMaturity(chainActive.Height()).nCoinbaseMaturity; i++)
     {
         std::vector<CMutableTransaction> noTxns;
         CBlock b = CreateAndProcessBlock(noTxns, scriptPubKey);
