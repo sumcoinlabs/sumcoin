eab3dd8b9df28593336da3391f816bfcbe7ef0f3
Sumcoin <37975862+sumcoinlabs@users.noreply.github.com>
2021-03-22 22:03:13 -0600

    reversal of previous adjustment

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -50,7 +50,7 @@ static const CAmount DEFAULT_FALLBACK_FEE = 200000;
 //! -discardfee default
 static const CAmount DEFAULT_DISCARD_FEE = 100000;
 //! -mintxfee default
-static const CAmount DEFAULT_TRANSACTION_MINFEE = 100000;
+static const CAmount DEFAULT_TRANSACTION_MINFEE = 10000;
 //! minimum recommended increment for BIP 125 replacement txs
 static const CAmount WALLET_INCREMENTAL_RELAY_FEE = 5000;
 //! Default for -spendzeroconfchange
