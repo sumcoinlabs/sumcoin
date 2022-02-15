48d9f87347c589eb63112994a251d9fbfcca2cc1
Mark McCorkle <1459355+markamccorkle@users.noreply.github.com>
2022-02-15 14:44:20 -0700

    Fix build error

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -177,7 +177,7 @@ public:
                 {0,         uint256S("0x000000d384b2c2ee13d10c0dc983052635e304ea8631136c81de0b76181891f5")},
                 {100,       uint256S("0x00000099242f8494ab53d675e5332a62bbd07c409ad0eb949142dd1c735552f3")},
                 {500,       uint256S("0x000000ae32bee2e5c9171a7cbc764546618529e26f7ce06fc2c5c3dcf1c9c939")},
-                {1000,      uint256S("0x0000009080a06d924ff436a891df8f83e538330073dee6cce7679cc0e7d6feb5")} // Last PoW
+                {1000,      uint256S("0x0000009080a06d924ff436a891df8f83e538330073dee6cce7679cc0e7d6feb5")}, // Last PoW
                 {1001,      uint256S("0xd867d95af28e1baa8e1b2b6a2db9c9491825e8067c46e9ca6fef8841746dc752")} // PoS
                 // {1016,      uint256S("0x91afaf0d08d45776e67fddeb82631acd1a351b760d5a1ee8f7b05493b1883b06")}
                 // {1284,      uint256S("0xbfd2f5e71d080d021e34334827b27f4236bcfb4b71a18aee328a14165068d817")}
