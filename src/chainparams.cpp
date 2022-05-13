0a3cf3e774ad04e0495724e79da38591e197781d
sumcoinlabs <sumcoinindex@gmail.com>
2022-05-13 13:22:16 -0600

    update params

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -130,16 +130,16 @@ public:
 
         // new parameters
         consensus.DiffMode = 1;  //always 1
-        consensus.DiffModeV2 = 1324010; //set to hieght for the new diffmode
-        consensus.SubnHeight = 1324015;
-        consensus.SubV = 1689970;
+        consensus.DiffModeV2 = 1323870; //set to height for the new diffmode
+        consensus.SubnHeight = 1323910;
+        consensus.SubV = 16903350.00000018;
         consensus.SubBlks = 10;
         consensus.nHeightEffective = 0;
         consensus.nCoinbaseMaturity = 10;
 
         // new tuneupconsensus parameters
         tuneupconsensus = consensus;
-        tuneupconsensus.nHeightEffective = 1323910;  //fork height after this block for coinbase maturity
+        tuneupconsensus.nHeightEffective = 1323900;  //fork height after this block for coinbase maturity
         tuneupconsensus.nCoinbaseMaturity = 100;
 
         // Assemble the binary search tree of parameters
