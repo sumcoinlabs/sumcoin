af83a8bdb6edc69e277bb9ee345c4c70fc1c6b3a
sumcoinlabs <sumcoinindex@gmail.com>
2023-01-02 05:36:47 -0700

    bech32_hrp = sum

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -162,7 +162,7 @@ public:
         base58Prefixes[EXT_SECRET_KEY] = {0xF5, 0x88, 0xAD, 0xE5};
 
         // human readable prefix to bench32 address
-        bech32_hrp = "sc";
+        bech32_hrp = "sum";
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
