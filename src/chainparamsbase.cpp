300f2f4f8cddc6b83fc2bdc470c0ac1c893d5d66
sumcoinlabs <sumcoinindex@gmail.com>
2023-06-17 15:50:27 -0600

    default rpc port to 3332

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -36,7 +36,7 @@ const CBaseChainParams& BaseParams()
 std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const std::string& chain)
 {
     if (chain == CBaseChainParams::MAIN)
-        return MakeUnique<CBaseChainParams>("", 9902);
+        return MakeUnique<CBaseChainParams>("", 3332);
     else if (chain == CBaseChainParams::TESTNET)
         return MakeUnique<CBaseChainParams>("testnet3", 9904);
     else if (chain == CBaseChainParams::REGTEST)
